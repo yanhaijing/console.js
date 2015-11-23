81b0d804e50e47fba6e0640b489c3edff2c4121d
yanhaijing <yanhaijing@yeah.net>
2015-11-23 14:06:41 +0800

    v0.2.0

--- console.js
+++ console.js
@@ -3,7 +3,7 @@
     var _console = g.console || {};
     var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'exception', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
 
-    var console = {version: '0.1.0'};
+    var console = {version: '0.2.0'};
     var key;
     for(var i = 0, len = methods.length; i < len; i++) {
         key = methods[i];
